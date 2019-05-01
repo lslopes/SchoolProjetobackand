3480fdbce4cbfe0bb4d51dc0d55fccfd2dc908f9
lslopes <43785503+lslopes@users.noreply.github.com>
2019-05-01 19:33:38 -0300

    COMMIT DISCIPLINAS
    COMMIT DISCIPLINAS FINAL

---
+++ disciplinas/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class discipilna(models.Model):
+    nomediscipilna = models.CharField("nomediscipilna", max_length=90)
+    descriçãodis = models.CharField("descrição", max_length=1000)
+    def __str__(self):
+        return self.nome
+
