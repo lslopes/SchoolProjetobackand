7b43b6f5b7d83dd308e225be65fde47d02512218
= <lucasjoserj@gmail.com>
2019-05-06 19:49:05 -0300

    final comit

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,5 +5,5 @@ class usuarios(models.Model):
     nomeuser = models.CharField("nome", max_length=90)
     senha = models.CharField("especialidades", max_length=90)
     def __str__(self):
-            return self.nome
+        return self.nome
 
