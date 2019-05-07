820ba474cc2293bfa7d2c54634427ed6fa8f7358
= <lucasjoserj@gmail.com>
2019-05-06 21:13:08 -0300

    pk

--- disciplinas/models.py
+++ disciplinas/models.py
@@ -4,5 +4,5 @@ class discipilna(models.Model):
     nomediscipilna = models.CharField("nomediscipilna", max_length=90)
     descriçãodis = models.CharField("descrição", max_length=1000)
     def __str__(self):
-        return self.nome
+        return self.nomediscipilna
 
